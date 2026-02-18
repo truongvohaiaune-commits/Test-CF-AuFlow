4cfcb237422206290634f0890fd2fb4c31dce4da
truongvohaiaune-commits <truongvohaiaune@gmail.com>
2026-02-18 12:53:23 +0700

    Update config.ts

--- services/config.ts
+++ services/config.ts
@@ -3,4 +3,4 @@
 // Change the URL string below to test with a different worker
 
 // @ts-ignore
-export const BACKEND_URL = (import.meta as any).env?.VITE_API_URL || "https://twilight-fire-b7d4.truongvohaiaune.workers.dev";
+export const BACKEND_URL = (import.meta as any).env?.VITE_API_URL || "https://backup.truongvohaiaune.workers.dev/";
